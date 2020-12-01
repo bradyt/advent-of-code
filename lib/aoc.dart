4735c4a8d7419270f9f043559fd3320cc1da2cc4
Brady Trainor <git_commit@tangential.info>
2020-12-01 00:00:00 +0000

    Shorten binary converter

--- lib/aoc.dart
+++ lib/aoc.dart
@@ -238,12 +238,10 @@ bool validatePassport(Map passport) {
           ));
 }
 
-int day05_convert(String boardingPass) =>
-    boardingPass
-        .split('')
-        .map((letter) => {'B': 1, 'F': 0, 'R': 1, 'L': 0}[letter])
-        .fold(0, (prev, element) => (prev + element) * 2) ~/
-    2;
+int day05_convert(String boardingPass) => boardingPass
+    .split('')
+    .map((letter) => {'B': 1, 'F': 0, 'R': 1, 'L': 0}[letter])
+    .fold(0, (prev, element) => 2 * prev + element);
 
 int day05(String contents) =>
     contents.trim().split('\n').map(day05_convert).reduce((x, y) => max(x, y));
