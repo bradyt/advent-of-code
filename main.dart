4a646902f8b9c66feaf29ca9d7eb9ae81bbd048b
Brady Trainor <git_commit@tangential.info>
2020-12-01 00:00:00 +0000

    Solve day 2

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  day01();
+  day02();
 }
 
 String getFileContents(String file) => File(file).readAsStringSync();
@@ -21,4 +21,36 @@ void day01() {
       }
     }
   }
+}
+
+void day02() {
+  var lines = getFileContents('day-02.txt')
+      .trim()
+      .split('\n');
+  // var lines = '1-3 a: abcde\n'
+  //         '1-3 b: cdefg\n'
+  //         '2-9 c: ccccccccc'
+  //     .split('\n');
+  var validPasswordCount = 0;
+  RegExp exp = RegExp(r'^(\d+)-(\d+) (\w): (\w*)$');
+  for (var line in lines) {
+    var match = exp.firstMatch(line);
+    var lowerBound = int.parse(match.group(1));
+    var upperBound = int.parse(match.group(2));
+    var letter = match.group(3);
+    var password = match.group(4);
+    var filteredPassword = password.replaceAll(RegExp('[^$letter]'), '');
+    var letterFrequency = filteredPassword.length;
+    var inBounds =
+        letterFrequency >= lowerBound && letterFrequency <= upperBound;
+    // print('*' * 80);
+    // print('\'$password\' ÷ \'$letter\' = \'$filteredPassword\'');
+    // print('$letterFrequency ' +
+    //     (inBounds ? '∈' : '∉') +
+    //     ' {$lowerBound, $upperBound}');
+    if (inBounds) {
+      validPasswordCount += 1;
+    }
+  }
+  print(validPasswordCount);
 }
