5f1007ba89161b38913c3314f2eaa913d5518031
Brady Trainor <git_commit@tangential.info>
2020-12-01 00:00:00 +0000

    Refactor

--- main.dart
+++ main.dart
@@ -1,9 +1,16 @@
 import 'dart:io';
 
 void main() {
-  File file = File('day-01.txt');
-  var entries =
-      file.readAsStringSync().trim().split('\n').map((str) => int.parse(str));
+  day01();
+}
+
+String getFileContents(String file) => File(file).readAsStringSync();
+
+void day01() {
+  var entries = getFileContents('day-01.txt')
+      .trim()
+      .split('\n')
+      .map((str) => int.parse(str));
   for (var i = 0; i < entries.length; i++) {
     var entry_i = entries.elementAt(i);
     for (var j = i + 1; j < entries.length; j++) {
